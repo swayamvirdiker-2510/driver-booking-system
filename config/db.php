4d012f1dc04c2d9243628434e4548e7ba13ec527
swayam virdiker <swayamvirdiker@gmail.com>
2026-08-13 11:22:49 +0530

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "driver_booking_system";
+
+$conn = new mysqli($host, $username, $password, $database);
+
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8mb4");
+
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+?>
